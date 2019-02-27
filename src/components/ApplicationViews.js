0db44d42c9a50907ce0304461d9f21e49a76dea7
nathan sepulveda <nathans.composer@gmail.com>
2019-02-27 13:06:38 -0600

    finished exercise

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -41,8 +41,9 @@
 import { Route } from 'react-router-dom'
 import React, { Component } from "react"
 import AnimalList from './animal/AnimalList'
-// import LocationList from './location/LocationList'
+import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
+import OwnersList from './owners/OwnersList'
 import { throws } from 'assert';
 
 
@@ -58,15 +59,22 @@ export default class ApplicationViews extends Component {
     componentDidMount() {
         const newState = {}
 
-        fetch("http://localhost:5002/animals")
+        fetch("http://localhost:5002/employees")
             .then(r => r.json())
-            .then(animals => newState.animals = animals)
-            .then(() => fetch("http://localhost:5002/employees")
-                .then(r => r.json()))
             .then(employees => newState.employees = employees)
+            .then(() => fetch("http://localhost:5002/locations")
+                .then(r => r.json()))
+            .then(locations => newState.locations = locations)
             .then(() => fetch("http://localhost:5002/owners")
                 .then(r => r.json()))
             .then(owners => newState.owners = owners)
+            .then(() => fetch("http://localhost:5002/ownerPets")
+                .then(r => r.json()))
+            .then(OwnersPets => newState.OwnersPets = OwnersPets)
+            .then(() => fetch("http://localhost:5002/animals")
+                .then(r => r.json()))
+            .then(animals => newState.animals = animals)
+            .then(() => this.setState(newState))
             .then(() => this.setState(newState))
     }
 
@@ -74,18 +82,18 @@ export default class ApplicationViews extends Component {
     render() {
         return (
             <React.Fragment>
-                {/* <Route exact path="/" render={(props) => {
+                <Route exact path="/" render={(props) => {
                     return <LocationList locations={this.state.locations} />
-                }} /> */}
-                <Route exact path="/animals" render={(props) => {
-                    return <AnimalList animals={this.state.animals} owners={this.state.owners} OwnersPets={throws.state.OwnersPets} />
                 }} />
                 <Route exact path="/employees" render={(props) => {
                     return <EmployeeList employees={this.state.employees} />
                 }} />
-                {/* <Route exact path="/owners" render={(props) => {
+                <Route path="/owners" render={(props) => {
                     return <OwnersList owners={this.state.owners} />
-                }} /> */}
+                }} />
+                <Route exact path="/animals" render={(props) => {
+                    return <AnimalList animals={this.state.animals} owners={this.state.owners} OwnersPets={this.state.OwnersPets} />
+                }} />
             </React.Fragment>
         )
     }
